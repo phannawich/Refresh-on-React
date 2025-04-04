7d98b100064c203b12d39145b15c076e90957bfb
Phannawich Jadpotwanich <phannawich1122@gmail.com>
2025-04-03 23:04:00 -0700

    finish

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
- 
+import LikeButton from './like-button';
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
@@ -7,22 +7,15 @@ function Header({ title }) {
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
  
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
- 
   return (
     <div>
-      <Header title="Develop. Preview. Ship." />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
- 
-      <button onClick={handleClick}>Like ({likes})</button>
+        <Header title="Develop. Preview. Ship." />
+        <ul>
+          {names.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+        <LikeButton />
     </div>
   );
 }
